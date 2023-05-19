0b0830d20abc887e2276b195bce579a8bfbd79ec
Marten Schälicke <m.schaelicke@s-v.de>
2023-05-19 10:32:49 +0200

    chore: align with up-to-date vue-create

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,6 +9,7 @@ module.exports = {
 
     extends: [
         'plugin:vue/vue3-essential',
+        'eslint:recommended',
         '@vue/eslint-config-typescript',
         '@vue/eslint-config-prettier',
         'plugin:promise/recommended',
